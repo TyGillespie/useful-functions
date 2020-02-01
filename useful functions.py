f335ca98c781f7ea1fac6951f3c1ab093fdbb2a1
TyGillespie <60036684+TyGillespie@users.noreply.github.com>
2020-02-01 16:56:10 -0700

    Added main script

---
+++ useful functions.py
@@ -0,0 +1,37 @@
+import ctypes
+import os
+import wx
+
+class console_class(object):
+
+	def __init__(self):
+		pass
+
+	def title(self,title):
+		ctypes.windll.kernel32.SetConsoleTitleW(title)
+
+	def output(self,text):
+		print(text)
+
+	def output_line(self,text):
+		print(text+"\n")
+
+	def clear(self):
+		os.system("cls")
+
+	def text_input(self,prompt=""):
+		return input(prompt)
+
+
+class gui_class(object):
+
+	def __init__(self):
+		pass
+
+	def alert(self,title,text):
+		wx.MessageBox(text,title)
+
+
+console=console_class()
+gui=gui_class()
+app=wx.App()
